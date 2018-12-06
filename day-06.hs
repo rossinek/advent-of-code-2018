a2eacdcff763a1fe042388ea90b95d5a786a9e1f
Artur Rosa <artur.rosa@monterail.com>
2018-12-06 22:34:44 +0100

    Day 06 - part 2

--- day-06.hs
+++ day-06.hs
@@ -3,15 +3,14 @@ import qualified Data.Map.Lazy as Map
 import Data.List.Extras.Argmax
 import Data.List.Split
 
-type Coordinate = (Int, Int)
-type Place = Coordinate
-type Board = Map.Map Coordinate (Maybe Place)
+type Place = (Int, Int)
+type Board a = Map.Map Place a
 
 parsePlace :: String -> Place
 parsePlace s = (read x :: Int, read y :: Int)
   where [x, y] = splitOn ", " s
 
-distance :: Coordinate -> Place -> Int
+distance :: Place -> Place -> Int
 distance (x, y) (x0, y0) = (abs (x0 - x)) + (abs (y0 - y))
 
 onlyOne :: [a] -> Maybe a
@@ -19,11 +18,17 @@ onlyOne l = if length l == 1
   then Just (head l)
   else Nothing
 
-placesMap :: Int -> Int -> [Place] -> Board
-placesMap w h places = Map.fromList
-  [((x, y), onlyOne (argmins (distance (x, y)) places)) | x <- [0..w+1], y <- [0..h+1]]
+placesMap :: Int -> Int -> (Place -> a) -> [Place] -> Board a
+placesMap w h f places = Map.fromList
+  [((x, y), f (x, y)) | x <- [0..w+1], y <- [0..h+1]]
 
-hasClosedArea :: Board -> Place -> Bool
+minUniqueDistanceMap :: Int -> Int -> [Place] -> Board (Maybe Place)
+minUniqueDistanceMap w h places = placesMap w h (\place -> onlyOne (argmins (distance place) places)) places
+
+totalDistanceMap :: Int -> Int -> [Place] -> Board Int
+totalDistanceMap w h places = placesMap w h (\place -> sum (map (distance place) places)) places
+
+hasClosedArea :: Board (Maybe Place) -> Place -> Bool
 hasClosedArea board place = not (any (\b -> (board Map.! b) == Just place) border)
   where
     left = minimum (map fst (Map.keys board))
@@ -36,7 +41,7 @@ hasClosedArea board place = not (any (\b -> (board Map.! b) == Just place) borde
       [(left, y) | y <- [1..(height-1)]] ++
       [(width, y) | y <- [1..(height-1)]]
 
-maxClosedArea :: Board -> [Place] -> Maybe Int
+maxClosedArea :: Board (Maybe Place) -> [Place] -> Maybe Int
 maxClosedArea board [] = Nothing
 maxClosedArea board places = if length placesWithClosedArea > 0
   then Just (maximum (map (\place -> length (filter (== place) (Map.elems board))) placesWithClosedArea))
@@ -50,5 +55,7 @@ main = do
   let places = map parsePlace (lines s)
   let width = maximum (map fst places)
   let height = maximum (map snd places)
-  let board = placesMap width height places
-  print $ maxClosedArea board places
+  let minDistBoard = minUniqueDistanceMap width height places
+  print $ maxClosedArea minDistBoard places
+  let totalDistBoard = totalDistanceMap width height places
+  print $ length (filter (<10000) (Map.elems totalDistBoard))
