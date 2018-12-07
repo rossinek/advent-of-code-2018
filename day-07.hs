9d3b9e08ee80b230377b545f1d575adca1d59aaf
Artur Rosa <artur.rosa@monterail.com>
2018-12-07 15:43:10 +0100

    Day 07 - part 2

--- day-07.hs
+++ day-07.hs
@@ -1,9 +1,9 @@
 import System.Environment
--- import qualified Data.Map.Lazy as Map
--- import Data.List.Extras.Argmax
--- import Data.List.Split
 import qualified Data.Set as Set
 import Data.List
+import Data.List.Extra (minimumOn)
+import Data.Char (ord)
+import Data.Graph.Inductive.Query.Monad (mapFst)
 
 type Task = Char
 type Dependency = (Task, Task)
@@ -29,10 +29,49 @@ tasksOrder deps tasks = (readyTask : tasksOrder (filter (not . (dependOn readyTa
   where
     readyTask = firstTask deps tasks
 
+type TaskWithDependencies = (Task, RemainingTime, [Task])
+type RemainingTime = Int
+type Workers = Int
+type Done = Task
+type InProgress = (Task, RemainingTime)
+type ToDo = TaskWithDependencies
+type Step = ([Done], [InProgress], [ToDo])
+
+readyQueue :: [TaskWithDependencies] -> [TaskWithDependencies]
+readyQueue tasks = sortOn (\(t, rt, deps) -> (length deps, t, rt)) readyTasks
+  where
+    readyTasks = filter (\(_,_,d) -> d == []) tasks
+
+groupDependencies :: [Dependency] -> [Task] -> [TaskWithDependencies]
+groupDependencies deps tasks = map (\t -> (t, reminingTime t, taskDependencies t)) tasks
+  where
+    taskDependencies :: Task -> [Task]
+    taskDependencies t = map fst (filter (isDependent t) deps)
+    reminingTime :: Task -> RemainingTime
+    reminingTime t = ((ord t) - 4)
+
+tick :: Workers -> Step -> [Step]
+tick w (done, [], []) = []
+tick w (d, ip, td) = (newStep : tick w newStep)
+  where
+    nextInProgress = map (\(t, rt) -> (t, (rt-1))) ip
+    (newDone, stillInProgress) = mapFst (map fst) (partition (\(_, rt) -> rt<=0) nextInProgress)
+    freeWorkers = w - (length stillInProgress)
+
+    nextToDo = map (\(t, rt, deps) -> (t, rt, deps \\ newDone )) td
+    willStart = take freeWorkers (readyQueue nextToDo)
+    stillToDo = nextToDo \\ willStart
+    newInProgress = map (\(t,rt,ts) -> (t,rt)) willStart
+
+    newStep = (d ++ newDone, stillInProgress ++ newInProgress, stillToDo)
+
+constructTimeline :: [TaskWithDependencies] -> Workers -> [Step]
+constructTimeline tasksWithDeps w = tick w ([], [], tasksWithDeps)
+
 main :: IO ()
 main = do
   s <- readFile "day-07.input"
   let dependencies = map parseDependency (lines s)
   let tasks = (Set.toList . Set.fromList) (map fst dependencies ++ map snd dependencies)
-  print $ tasks
   print $ tasksOrder dependencies tasks
+  print $ (length (constructTimeline (groupDependencies dependencies tasks) 5)) - 1
