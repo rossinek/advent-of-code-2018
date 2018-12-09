63f3d609f164eb63b71f8328bf63702398dabce0
Artur Rosa <artur.rosa@monterail.com>
2018-12-09 12:39:43 +0100

    Day 09 - with CList - part 2

--- day-09.hs
+++ day-09.hs
@@ -1,10 +1,6 @@
 import System.Environment
-
-type Gameplay = Int -> (Marbles, Scores)
-type Player = Int
-type Score = Int
-type Scores = [(Player, Score)]
-type Marbles = [Int]
+import Data.CircularList
+import Data.Maybe (fromJust)
 
 parseInput :: String -> (Int, Int)
 parseInput s = (players, lastMarble)
@@ -12,33 +8,32 @@ parseInput s = (players, lastMarble)
     players = read ((words s) !! 0)
     lastMarble = read ((words s) !! 6)
 
+type Gameplay = Int -> Turn
+type Turn = (Marbles, Scores)
+type Scores = CList Int
+type Marbles = CList Int
+
 gameplayFor :: Int -> Gameplay
 gameplayFor nplayers = gameplay
   where
     gameplay :: Gameplay
-    gameplay n  | n == 0            = ([0], [(p, 0) | p <- [1..nplayers]])
+    gameplay n  | n == 0            = (fromList [0], fromList [0 | p <- [1..nplayers]])
                 | (n `mod` 23) == 0 = specialMove n (gameplay (n-1))
                 | otherwise         = standardMove n (gameplay (n-1))
 
-    standardMove :: Int -> (Marbles, Scores) -> (Marbles, Scores)
-    standardMove n (board, (ps:pss)) = (next board, pss ++ [ps])
+    standardMove :: Int -> Turn -> Turn
+    standardMove n (marbles, scores) = (insertL n (rotR marbles), rotR scores)
+
+    specialMove :: Int -> Turn -> Turn
+    specialMove n (marbles, scores) = (newMarbles, rotR (update newScore scores))
       where
-        next :: Marbles -> Marbles
-        next (x:y:tail) = (n:tail) ++ [x, y]
-        next xs = (n:xs)
+        rotatedMarbles = rotNL 7 marbles
+        removedMarble = fromJust (focus rotatedMarbles)
+        newMarbles = removeR rotatedMarbles
+        newScore = (fromJust (focus scores)) + removedMarble + n
 
-    specialMove :: Int -> (Marbles, Scores) -> (Marbles, Scores)
-    specialMove n (board, ((p, s):pss)) = (nextBoard, pss ++ [(p, newScore)])
-      where
-        (m:nextBoard) = next board
-        newScore = s + n + m
-
-        next :: Marbles -> Marbles
-        next board = b2 ++ b1
-          where (b1, b2) = splitAt (length board - 7) board
-
-highscore :: Int -> Int -> Score
-highscore players lastMarble = maximum (map snd scores)
+highscore :: Int -> Int -> Int
+highscore players lastMarble = maximum (toList scores)
   where
     scores = (snd (gameplayFor players lastMarble))
 
@@ -47,4 +42,4 @@ main = do
   s <- readFile "input/day-09.input"
   let (players, lastMarble) = parseInput s
   print $ highscore players lastMarble
-  -- print $ highscore players (lastMarble * 100)
+  print $ highscore players (lastMarble * 100)
