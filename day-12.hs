0e7468bb34b0f0467f861bae3d4f899520755655
Artur Rosa <artur.rosa@monterail.com>
2018-12-12 17:28:00 +0100

    Day 12 - part 2

--- day-12.hs
+++ day-12.hs
@@ -13,11 +13,14 @@ parseInput s = (state, rulesMap rules)
     ls = lines s
     state = map toBool (drop 15 (head ls))
     rules = map toRule (map (splitOn " => ") (drop 2 ls))
+
     toBool :: Char -> Bool
     toBool '#' = True
     toBool '.' = False
+
     toRule :: [String] -> Rule
     toRule (pattern:out:[]) = (toPattern pattern, toBool (head out))
+
     toPattern :: [Char] -> Pattern
     toPattern (a:b:c:d:e:[]) = (toBool a, toBool b, toBool c, toBool d, toBool e)
 
@@ -25,22 +28,46 @@ rulesMap :: [Rule] -> RulesMap
 rulesMap rules = foldl addRule Map.empty rules
   where
     addRule :: RulesMap -> Rule -> RulesMap
-    addRule rmap (pattern, out) = Map.insert pattern out rmap
+    addRule rmap (pattern, out) = if out
+      then Map.insert pattern out rmap
+      else rmap
+
+patternFromList :: [Bool] -> (Bool, Bool, Bool, Bool, Bool)
+patternFromList (a:b:c:d:e:xs)  = (a, b, c, d, e)
+patternFromList (a:b:c:d:xs)    = (a, b, c, d, False)
+patternFromList (a:b:c:xs)      = (a, b, c, False, False)
+patternFromList (a:b:xs)        = (a, b, False, False, False)
+patternFromList (a:xs)          = (a, False, False, False, False)
+patternFromList []              = (False, False, False, False, False)
 
 nextGeneration :: RulesMap -> (Int, Pots) -> (Int, Pots)
-nextGeneration rmap (start, pots) = (start-2, nextGeneration' (False:False:False:False:pots))
+nextGeneration rmap (start, pots) = trimBegin (start-1, nextGeneration' (False:False:False:pots))
   where
     nextGeneration' :: Pots -> Pots
     nextGeneration' [] = []
-    nextGeneration' (d:[]) = ((rmap Map.! (d,False,False,False,False)):nextGeneration' [])
-    nextGeneration' (c:d:[]) = ((rmap Map.! (c,d,False,False,False)):nextGeneration' (d:[]))
-    nextGeneration' (b:c:d:[]) = ((rmap Map.! (b,c,d,False,False)):nextGeneration' (c:d:[]))
-    nextGeneration' (b:c:d:e:[]) = ((rmap Map.! (b,c,d,e,False)):nextGeneration' (c:d:e:[]))
-    nextGeneration' (a:b:c:d:e:ps) = ((rmap Map.! (a,b,c,d,e)):nextGeneration' (b:c:d:e:ps))
+    nextGeneration' pots = ((getV (patternFromList pots)):nextGeneration' (tail pots))
+
+    getV :: Pattern -> Bool
+    getV p = Map.findWithDefault False p rmap
+
+    trimBegin :: (Int, Pots) -> (Int, Pots)
+    trimBegin (n, (False:pots)) = trimBegin (n+1,pots)
+    trimBegin x = x
+
+equalPots :: Pots -> Pots -> Bool
+equalPots (x:xs) (y:ys) = (x == y) && (equalPots xs ys)
+equalPots [] xs = foldl (\acc v -> acc && (v == False)) True xs
+equalPots xs [] = foldl (\acc v -> acc && (v == False)) True xs
 
 nextNGeneration :: RulesMap -> Int -> (Int, Pots) -> (Int, Pots)
 nextNGeneration rmap 0 (s, ps) = (s, ps)
-nextNGeneration rmap n (s, ps) = nextNGeneration rmap (n-1) (nextGeneration rmap (s,ps))
+nextNGeneration rmap n (s, ps) = nextNGeneration' (n-1) (s, ps) (nextGeneration rmap (s,ps))
+  where
+    nextNGeneration' :: Int -> (Int, Pots) -> (Int, Pots) -> (Int, Pots)
+    nextNGeneration' 0 prev current = current
+    nextNGeneration' m' (prevPos, prevGen) current@(currPos, currGen) = if equalPots prevGen currGen
+      then (currPos + (currPos - prevPos) * m', currGen)
+      else nextNGeneration' (m'-1) current (nextGeneration rmap current)
 
 sumPotsNumbers :: (Int, Pots) -> Int
 sumPotsNumbers (_, []) = 0
@@ -48,9 +75,9 @@ sumPotsNumbers (i, (p:ps)) = if p
   then i + sumPotsNumbers (i+1, ps)
   else sumPotsNumbers (i+1, ps)
 
-
 main :: IO ()
 main = do
   s <- readFile "input/day-12.input"
   let (pots, rMap) = parseInput s
   print $ sumPotsNumbers (nextNGeneration rMap 20 (0, pots))
+  print $ sumPotsNumbers (nextNGeneration rMap 50000000000 (0, pots))
